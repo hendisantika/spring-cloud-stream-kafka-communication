dcd32b36cb614626e77f41c27aef2e1e98de1c0e
hendisantika <hendisantika@yahoo.co.id>
2024-06-18 18:43:06 +0700

    Create Spring Boot Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/cloudstream/SpringCloudStreamKafkaCommunicationApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.cloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamKafkaCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamKafkaCommunicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/cloudstream/SpringCloudStreamKafkaCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.cloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamKafkaCommunicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
