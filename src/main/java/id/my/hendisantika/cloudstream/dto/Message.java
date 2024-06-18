b0c28a7fb25cd81e8e9ad2ed2adcc8c6ab9a2299
hendisantika <hendisantika@yahoo.co.id>
2024-06-18 18:48:52 +0700

    Add Message DTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/cloudstream/dto/Message.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.cloudstream.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-stream-kafka-communication
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/18/24
+ * Time: 18:48
+ * To change this template use File | Settings | File Templates.
+ */
+public record Message(String name) {
+}
