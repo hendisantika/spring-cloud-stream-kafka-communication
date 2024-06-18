50aaa4be347ce4b41702e60c1b2591bdaf0bd9a8
hendisantika <hendisantika@yahoo.co.id>
2024-06-18 18:52:00 +0700

    Add consumer Bean🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/cloudstream/SpringCloudStreamKafkaCommunicationApplication.java
+++ src/main/java/id/my/hendisantika/cloudstream/SpringCloudStreamKafkaCommunicationApplication.java
@@ -1,13 +1,26 @@
 package id.my.hendisantika.cloudstream;
 
+import id.my.hendisantika.cloudstream.dto.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.function.Consumer;
+
+@Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class SpringCloudStreamKafkaCommunicationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudStreamKafkaCommunicationApplication.class, args);
+    }
+
+    @Bean
+    public Consumer<Message> consumer() {
+        return message -> log.info("received {}", message);
     }
 
 }
